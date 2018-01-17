da3c42a9110a227697405e0e75d34e701703fbd6
Michael Evans <michael@michaelgregoryevans.com>
2018-01-17 21:21:24 +0000

    added code to assign session[:user_id] after successful sign up.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,6 +8,7 @@ class UserController < ApplicationController
     user = User.create(params[:user])
 
     if user.save
+      session[:user_id] = user.id
       redirect to '/characters'
     else
       redirect to '/signup'
