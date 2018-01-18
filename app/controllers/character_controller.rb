861d82feb4dc42c75aba18eff593e571d971b629
Michael Evans <michael@michaelgregoryevans.com>
2018-01-18 17:16:45 +0000

    edited characters/new.erb name='class' -> name='char_class'. added code to post '/characters' to make new character based on params, assign user_id from session, save character.

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -7,4 +7,10 @@ class CharacterController < ApplicationController
   get '/characters/new' do
     erb :'/characters/new'
   end
+
+  post '/characters' do
+    character = Character.new(params[:character])
+    character.user_id = session[user_id]
+    character.save
+  end
 end
