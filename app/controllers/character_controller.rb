a419a84db6fcf6e6978e6903defbc80bba5e5272
Michael Evans <michael@michaelgregoryevans.com>
2018-01-18 17:20:00 +0000

    wrote code for get '/characters/:id' that find characters by params[:id] and calls characters/show.erb

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -12,5 +12,13 @@ class CharacterController < ApplicationController
     character = Character.new(params[:character])
     character.user_id = session[user_id]
     character.save
+
+    redirect to "/characters/#{character.id}"
+  end
+
+  get '/characters/:id' do
+    @character = Character.find(params[:id])
+
+    erb :'/characters/show'
   end
 end
